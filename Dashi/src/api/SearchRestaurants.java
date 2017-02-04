a8724f132e31af54a6281547da0c88a7c6e2d795
brucexiejiaming <brucexiejiaming@gmail.com>
2017-02-04 16:17:15 -0500

    Yelp API + MySQL

--- Dashi/src/api/SearchRestaurants.java
+++ Dashi/src/api/SearchRestaurants.java
@@ -2,6 +2,7 @@ package api;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.PreparedStatement;
 import java.util.*;
 
 import javax.servlet.ServletException;
@@ -13,6 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import db.DBConnection;
+import db.MySQLDBConnection;
+import yelp.YelpAPI;
 
 /**
  * Servlet implementation class SearchRestaurants
@@ -34,20 +39,34 @@ public class SearchRestaurants extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		JSONArray array = new JSONArray();
-		try {
-			if (request.getParameterMap().containsKey("user_id")
-					&& request.getParameterMap().containsKey("lat")
-					&& request.getParameterMap().containsKey("lon")) {
-				String userId = request.getParameter("user_id");
-				double lat = Double.parseDouble(request.getParameter("lat"));
-				double lon = Double.parseDouble(request.getParameter("lon"));
-				// return some fake restaurants
-				array.put(new JSONObject().put("name", "Panda Express"));
-				array.put(new JSONObject().put("name", "Hong Kong Express"));
-			}
-		} catch (JSONException e) {
-			e.printStackTrace();
+//		JSONArray array = new JSONArray();
+//		try {
+//			if (request.getParameterMap().containsKey("user_id")
+//					&& request.getParameterMap().containsKey("lat")
+//					&& request.getParameterMap().containsKey("lon")) {
+//				String userId = request.getParameter("user_id");
+//				double lat = Double.parseDouble(request.getParameter("lat"));
+//				double lon = Double.parseDouble(request.getParameter("lon"));
+//				// return some fake restaurants
+//				array.put(new JSONObject().put("name", "Panda Express"));
+//				array.put(new JSONObject().put("name", "Hong Kong Express"));
+//			}
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//		}
+//		RpcParser.writeOutput(response, array);
+		
+        JSONArray array = new JSONArray();
+		DBConnection connection = new MySQLDBConnection();
+		if (request.getParameterMap().containsKey("lat")
+				&& request.getParameterMap().containsKey("lon")) {
+			// term is null or empty by default
+			String term = request.getParameter("term");
+			//String userId = (String) session.getAttribute("user");
+		                                   String userId = "1111";
+			double lat = Double.parseDouble(request.getParameter("lat"));
+			double lon = Double.parseDouble(request.getParameter("lon"));
+			array = connection.searchRestaurants(userId, lat, lon, term);
 		}
 		RpcParser.writeOutput(response, array);
 	}
